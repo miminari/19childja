4775d284a10601d2cbb386bcf1885311fbc349cb
miminari <mimitips@gmail.com>
2019-03-24 16:01:58 +0900

    init

---
+++ functions.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Twenty Nineteen の子テーマサンプルです。
+ */
+
+//style.cssの読み込み
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_parent_theme_file_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_theme_file_uri() . '/style.css' );
+}
+
+//カラーパレットと文字サイズメニューの設定
+function setup_editor_menu_for_19child() {
+    //カラーパレット
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => 'メインカラー',
+            'slug' => 'primary',
+            'color' => '#CE2121',
+        ),
+        array(
+            'name' => 'サブカラー',
+            'slug' => 'secondary',
+            'color' => '#988768',
+        ),
+        array(
+            'name' => '背景用',
+            'slug' => 'background',
+            'color' => '#000',
+        )
+    ) );
+    add_theme_support( 'editor-font-sizes', array(
+        array(
+            'name' => __( 'Small', 'themeLangDomain' ),
+            'size' => 12,
+            'slug' => 'small'
+        ),
+        array(
+            'name' => __( 'Normal', 'themeLangDomain' ),
+            'size' => 14,
+            'slug' => 'normal'
+        ),
+        array(
+            'name' => __( 'Large', 'themeLangDomain' ),
+            'size' => 21,
+            'slug' => 'large'
+        ),
+        array(
+            'name' => __( 'Huge', 'themeLangDomain' ),
+            'size' => 36,
+            'slug' => 'huge'
+        )
+    ) );
+
+    //カスタムカラーをオフにする設定
+    //add_theme_support( 'disable-custom-colors' );
+
+    //文字サイズの数値指定をオフにする設定
+    add_theme_support( 'disable-custom-font-sizes' );
+
+    // Add support for editor styles.
+    add_theme_support( 'editor-styles' );
+
+    // Enqueue editor styles.
+    add_editor_style( 'editor-style-child.css' );
+
+ }
+add_action( 'after_setup_theme', 'setup_editor_menu_for_19child' ,11);
+
+
+
+
+
+
+?>
